1a874df4bf25a49329c488597c7e3ada6b8cd867
annafromthecfam <anna@myclarkfamily.org>
2022-10-13 17:08:19 -0600

    commit01

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿void StackNoParams()
+{
+    int a;
+    long b;
+    float c;
+    double d;
+    decimal e;
+}
+
+void StackWithParams(int a, int b, int c)
+{
+
+}
+
+void StackWithHeap(int a, int b, int c, string d)
+{
+    int f;
+    long g;
+    int[] h;
+}
+
+void StackOver(long a)
+{
+    StackOver(472874932);
+}
+
+StackNoParams();
+Console.WriteLine("StackNoParams has executed.");
+Console.ReadKey();
+
+StackWithParams(12, 24, 53);
+Console.WriteLine("StackWithParams has executed.");
+Console.ReadKey();
+
+StackWithHeap(53, 23, 74832, "Pizza");
+Console.WriteLine("StackWithHeap has executed.");
+Console.ReadKey();
+
+StackOver(2535);
